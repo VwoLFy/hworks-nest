9742112083b1d0711eb7b370f7535dbc9a1e2505
VwoLFy <v_wolfy@mail.ru>
2023-01-21 21:28:04 +0300

    fix user schema for mongoose

--- src/users/domain/user.schema.ts
+++ src/users/domain/user.schema.ts
@@ -4,31 +4,68 @@ import { ObjectId } from 'mongodb';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { randomUUID } from 'crypto';
 
+@Schema({ _id: false })
 export class AccountData {
+  @Prop({ required: true })
   createdAt: Date;
 
-  constructor(public login: string, public passwordHash: string, public email: string) {
+  @Prop({
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+    validate: (val: string) => {
+      return val.match('^[a-zA-Z0-9_-]*$');
+    },
+  })
+  login: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  @Prop({
+    required: true,
+    validate: (val: string) => {
+      return val.match('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+    },
+  })
+  email: string;
+
+  constructor(login: string, passwordHash: string, email: string) {
     this.createdAt = new Date();
+    this.login = login;
+    this.passwordHash = passwordHash;
+    this.email = email;
   }
 }
+export const AccountDataSchema = SchemaFactory.createForClass(AccountData);
+
+@Schema({ _id: false })
 export class EmailConfirmation {
+  @Prop({ required: true })
   confirmationCode: string;
+
+  @Prop({ required: true })
   expirationDate: Date;
 
-  constructor(public isConfirmed: boolean) {
+  @Prop({ required: true })
+  isConfirmed: boolean;
+
+  constructor(isConfirmed: boolean) {
     this.confirmationCode = randomUUID();
     this.expirationDate = add(new Date(), { hours: 1 });
+    this.isConfirmed = isConfirmed;
   }
 }
+export const EmailConfirmationSchema = SchemaFactory.createForClass(EmailConfirmation);
 
 @Schema()
 export class User {
   _id: ObjectId;
 
-  @Prop({ required: true, type: AccountData })
+  @Prop({ required: true, type: AccountDataSchema })
   accountData: AccountData;
 
-  @Prop({ required: true, type: EmailConfirmation })
+  @Prop({ required: true, type: EmailConfirmationSchema })
   emailConfirmation: EmailConfirmation;
 
   confirmUser() {
