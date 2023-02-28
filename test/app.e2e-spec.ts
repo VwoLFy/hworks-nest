f8267b9e15bde89fecce7fb629639f4bc5bce065
VwoLFy <v_wolfy@mail.ru>
2023-02-28 22:03:29 +0300

    vercel

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -52,7 +52,7 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
+  it('/(GET)', () => {
     return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 
