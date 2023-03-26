667df829693ab33ec26f194e2ea99765ede911dc
VwoLFy <v_wolfy@mail.ru>
2023-03-26 23:14:06 +0300

    add sort questions in view model for find games

--- src/modules/quiz-game/api/models/GamePairViewModel.ts
+++ src/modules/quiz-game/api/models/GamePairViewModel.ts
@@ -19,7 +19,9 @@ export class GamePairViewModel {
     this.firstPlayerProgress = new GamePlayerProgressViewModel(game.players[0]);
     this.secondPlayerProgress = game.players[1] ? new GamePlayerProgressViewModel(game.players[1]) : null;
     this.questions =
-      game.questions && game.questions.length > 0 ? game.questions.map((q) => new QuestionInGameViewModel(q)) : null;
+      game.questions && game.questions.length > 0
+        ? game.questions.sort((a, b) => a.id - b.id).map((q) => new QuestionInGameViewModel(q))
+        : null;
     this.status = game.status;
     this.pairCreatedDate = game.pairCreatedDate.toISOString();
     this.startGameDate = game.startGameDate ? game.startGameDate.toISOString() : null;
